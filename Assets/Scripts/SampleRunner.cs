7edf40655e10c96b241d97a622aa538e029843e7
mattak <mattak.me@gmail.com>
2025-11-21 18:04:04 +0900

    ✨ feat(SampleRunner): add UsefulSample async demo showing UniTask, coroutine & UI integrations

--- Assets/Scripts/SampleRunner.cs
+++ Assets/Scripts/SampleRunner.cs
@@ -1,8 +1,12 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Triggers;
+using Mono.Cecil;
 using Sample;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class SampleRunner : MonoBehaviour
 {
@@ -84,5 +88,36 @@ public class SampleRunner : MonoBehaviour
         Debug.Log("RunTask: Start");
         throw new System.Exception("Exception on RunTask");
         Debug.Log("RunTask: End");
+    }
+
+    private async UniTaskVoid UsefulSample()
+    {
+        // Coroutine連携が簡単
+        var request = UnityWebRequest.Get("https://google.com");
+        await request.SendWebRequest(); // IEnumeratorで定義したCoroutineをそのままUniTask実行できる
+        var resource = await Resources.LoadAsync<GameObject>("Sample");
+        
+        // Coroutineとのキャンセル統合も用意
+        var token = this.GetCancellationTokenOnDestroy(); // OnDestroyで自動キャンセル
+        await request.SendWebRequest().ToUniTask(cancellationToken: token); // キャンセル連携も簡単
+
+        // Unity似合わせた時間処理も簡単 (1frameだけ待つ処理も簡単)
+        await UniTask.Yield();
+        await UniTask.NextFrame();
+        await UniTask.DelayFrame(1);
+        
+        // UniTaskはmainThread実行なのでasync内部でのUI操作が安心して行える
+        TMP_Text text = GetComponentInChildren<TMP_Text>();
+        if (text != null) text.text = "Hello World!";
+        
+        // Unityコンポーネントとの関数連携が簡単
+        Collision collision = await this.GetAsyncCollisionEnterTrigger().OnCollisionEnterAsync(token);
+        UnityEngine.UI.Button button =  GetComponent<UnityEngine.UI.Button>();
+        await button.OnClickAsync(token);
+        
+        // その他便利関数多数
+        await UniTask.WaitUntil(() => transform.position.y > 0);
+        await UniTask.WaitUntilValueChanged(transform, x => x.position.y);
+        // ...
     }
 }
